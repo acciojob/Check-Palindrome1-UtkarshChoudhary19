46754519107ecf55b24db533de6b66a2c6534c7a
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-06-16 17:54:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-	str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+	let str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 	let j = str.length - 1;
     for (let i = 0; i < j / 2; i++) {
         let x = str[i];
